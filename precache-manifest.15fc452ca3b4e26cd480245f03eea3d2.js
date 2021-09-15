064dc8ec2fad6676c543061e51b30aab77cbeb2c
Anirudh Kumar Subramanyam <ask710@uw.edu>
2021-09-15 13:34:13 -0400

    Updates

---
+++ precache-manifest.15fc452ca3b4e26cd480245f03eea3d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b6005d6b1be43485ac575fcaf288b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f23b10030eaeeb33e9e0",
+    "url": "./static/css/main.c21098ee.chunk.css"
+  },
+  {
+    "revision": "11db377173537acbcf8d",
+    "url": "./static/js/2.c9c54850.chunk.js"
+  },
+  {
+    "revision": "f23b10030eaeeb33e9e0",
+    "url": "./static/js/main.d0056d18.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- src/Routes.js
+++
@@ -1,5 +0,0 @@
-export const Routes = {
-  Home: "/",
-  Projects: "/projects",
-  Experiences: "/experiences", 
-}
